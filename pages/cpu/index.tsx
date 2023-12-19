b2dcea5c385d306a7016307e43fd770c217f523d
AdrianOles <olesnieadrian@gmail.com>
2023-12-19 08:05:43 -0500

    fixing the images

--- pages/cpu/index.tsx
+++ pages/cpu/index.tsx
@@ -10,6 +10,7 @@ const figtree = Figtree({ subsets: ['latin'] })
 
 export default function Home() {
     const router = useRouter();
+    const loaderProp = ({ src } : any) => { return src }
 
     const cpuChange = (res: any) => {
         if (res === "101010") {
@@ -30,6 +31,7 @@ export default function Home() {
                 className='absolute z-50 top-24 right-24 opacity-[70%]'
                 width={100}
                 height={100}
+                loader={loaderProp}
             />
 
 
